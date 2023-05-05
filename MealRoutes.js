08653067522e40ae5f9572a6f67c0105dc691dac
Maria Sokolova <92234362+mamizheva85@users.noreply.github.com>
2023-05-05 10:42:09 -0400

    Update MealRoutes.js

--- MealRoutes.js
+++ MealRoutes.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/', getMeal);
 router.post('/saveMeals', saveMeals);
-router.delete('/deleteMeal', deleteMeal);
-router.put('/editMeal', editMeal)
+router.post('/deleteMeal', deleteMeal);
+router.post('/editMeal', editMeal);
 
 module.exports = router;
